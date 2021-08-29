549558142de172059bdd739fd0dff2498b5a3063
m5r <mokht@rmi.al>
2021-08-29 21:10:25 +0800

    parallelize metric fetching requests

--- app/public-area/queries/get-metrics.ts
+++ app/public-area/queries/get-metrics.ts
@@ -1,16 +1,19 @@
 import { resolver } from "blitz";
+
 import db from "../../../db";
 
 export default resolver.pipe(async () => {
-	const phoneNumbers = await db.phoneNumber.count();
-	const smsExchanged = await db.message.count();
-	const allPhoneCalls = await db.phoneCall.findMany();
-	const secondsCalled = allPhoneCalls.reduce<number>((minutes, phoneCall) => {
+	const [phoneNumbers, smsExchanged, allPhoneCalls] = await Promise.all([
+		db.phoneNumber.count(),
+		db.message.count(),
+		db.phoneCall.findMany(),
+	]);
+	const secondsCalled = allPhoneCalls.reduce<number>((seconds, phoneCall) => {
 		if (!phoneCall.duration) {
-			return minutes;
+			return seconds;
 		}
 
-		return minutes + Number.parseInt(phoneCall.duration);
+		return seconds + Number.parseInt(phoneCall.duration);
 	}, 0);
 	const minutesCalled = Math.round(secondsCalled / 60);
 
