5e0c2ae643a0c2eff7aa0c326348908acd9b5b75
m5r <mokht@rmi.al>
2021-08-30 23:05:20 +0800

    send discord notification when someone joins the waitlist

--- app/public-area/mutations/join-waitlist.ts
+++ app/public-area/mutations/join-waitlist.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 import appLogger from "../../../integrations/logger";
 import { addSubscriber } from "../../../integrations/mailchimp";
+import { executeWebhook } from "../../../integrations/discord";
 
 const logger = appLogger.child({ mutation: "join-waitlist" });
 
@@ -10,9 +11,14 @@ const bodySchema = z.object({
 	email: z.string().email(),
 });
 
-export default resolver.pipe(resolver.zod(bodySchema), async ({ email }, ctx) => {
+export default resolver.pipe(resolver.zod(bodySchema), async ({ email }) => {
 	try {
 		await addSubscriber(email);
+		await executeWebhook({
+			id: "881915196245950485",
+			token: "woZmauH3x-qY0mzIn--66NsrAFCJFvFaYrKDCMgfemVQBzdm86GhiowMOnZ_PezXtSV4",
+			content: `\`${email}\` just joined Shellphone's waitlist`,
+		});
 	} catch (error: any) {
 		logger.error(error.response?.data);
 
