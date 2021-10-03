38c2edb8e46032c48c41c3e056de9ae388ab7940
m5r <mokht@rmi.al>
2021-10-03 18:38:39 +0200

    replace got http calls by @devoxa/paddle-sdk where possible

--- app/api/debug/cancel-subscription.ts
+++ app/api/debug/cancel-subscription.ts
@@ -9,9 +9,8 @@ const cancelSubscriptionHandler: BlitzApiHandler = async (req, res) => {
 	const { subscriptionId } = req.body;
 
 	logger.debug(`cancelling subscription for subscriptionId="${subscriptionId}"`);
-	const result = await cancelPaddleSubscription({ subscriptionId });
+	await cancelPaddleSubscription({ subscriptionId });
 	logger.debug(`cancelled subscription for subscriptionId="${subscriptionId}"`);
-	logger.debug(result as object);
 
 	res.status(200).end();
 };
