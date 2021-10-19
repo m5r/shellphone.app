59b13bd0ea171c5b7caceb9b4d381e8a0de7a1e1
m5r <mokht@rmi.al>
2021-10-19 23:56:16 +0200

    blur phone call history if on free plan

--- app/core/layouts/layout/footer.tsx
+++ app/core/layouts/layout/footer.tsx
@@ -6,7 +6,7 @@ import clsx from "clsx";
 export default function Footer() {
 	return (
 		<footer
-			className="grid grid-cols-4 bg-[#F7F7F7] border-t border-gray-400 border-opacity-25 py-3"
+			className="grid grid-cols-4 bg-[#F7F7F7] border-t border-gray-400 border-opacity-25 py-3 z-10"
 			style={{ flex: "0 0 50px" }}
 		>
 			<NavLink label="Calls" path="/calls" icon={<IoCall className="w-6 h-6" />} />
--- app/phone-calls/pages/calls.tsx
+++ app/phone-calls/pages/calls.tsx
@@ -3,20 +3,37 @@ import type { BlitzPage } from "blitz";
 import { Routes } from "blitz";
 
 import AppLayout from "app/core/layouts/layout";
-import PhoneCallsList from "../components/phone-calls-list";
 import MissingTwilioCredentials from "app/core/components/missing-twilio-credentials";
 import useCurrentUser from "app/core/hooks/use-current-user";
-import PageTitle from "../../core/components/page-title";
-import Spinner from "../../core/components/spinner";
+import PageTitle from "app/core/components/page-title";
+import Spinner from "app/core/components/spinner";
+import InactiveSubscription from "app/core/components/inactive-subscription";
+import PhoneCallsList from "../components/phone-calls-list";
 
 const PhoneCalls: BlitzPage = () => {
-	const { hasFilledTwilioCredentials, hasPhoneNumber } = useCurrentUser();
+	const { hasFilledTwilioCredentials, hasPhoneNumber, hasActiveSubscription } = useCurrentUser();
 
 	if (!hasFilledTwilioCredentials || !hasPhoneNumber) {
 		return (
 			<>
 				<MissingTwilioCredentials />
 				<PageTitle className="filter blur-sm select-none absolute top-0" title="Calls" />
+			</>
+		);
+	}
+
+	if (!hasActiveSubscription) {
+		return (
+			<>
+				<InactiveSubscription />
+				<div className="filter blur-sm select-none absolute top-0 w-full h-full z-0">
+					<PageTitle title="Calls" />
+					<section className="relative flex flex-grow flex-col">
+						<Suspense fallback={<Spinner />}>
+							<PhoneCallsList />
+						</Suspense>
+					</section>
+				</div>
 			</>
 		);
 	}
