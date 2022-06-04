59430445098daab1c6d17507f0272ba84bac84dc
m5r <mokht@rmi.al>
2022-06-04 23:34:33 +0200

    cache loader data and document, network-first approach

--- app/service-worker/fetch.ts
+++ app/service-worker/fetch.ts
@@ -1,12 +1,25 @@
-import { ASSET_CACHE, cacheAsset } from "~/service-worker/cache-utils";
+import {
+	cacheAsset,
+	cacheDocument,
+	cacheLoaderData,
+	isAssetRequest,
+	isDocumentGetRequest,
+	isLoaderRequest,
+} from "~/service-worker/cache-utils";
 
 declare let self: ServiceWorkerGlobalScope;
 
-export type FetchEventWithPreloadResponse = FetchEvent & { preloadResponse?: Promise<Response | undefined> };
-
-export default async function handleFetch(event: FetchEventWithPreloadResponse) {
-	if (["font", "image", "script", "style"].includes(event.request.destination)) {
+export default async function handleFetch(event: FetchEvent) {
+	if (isAssetRequest(event.request)) {
 		return cacheAsset(event);
+	}
+
+	if (isLoaderRequest(event.request)) {
+		return cacheLoaderData(event);
+	}
+
+	if (isDocumentGetRequest(event.request)) {
+		return cacheDocument(event);
 	}
 
 	return fetch(event.request);
